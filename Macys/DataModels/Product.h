b310cf643e88dc2479208542be86ce1003e5c54b
Max Makankov <mmax@t-sk.ru>
2014-06-04 14:13:51 +0400

    - Added submodule FMDB

---
+++ Macys/DataModels/Product.h
@@ -0,0 +1,13 @@
+//
+//  MCProduct.h
+//  Macys
+//
+//  Created by mmakankov on 04/06/14.
+//  Copyright (c) 2014 mmakankov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MCProduct : NSObject
+
+@end
---
+++ Macys/DataModels/Product.m
@@ -0,0 +1,13 @@
+//
+//  MCProduct.m
+//  Macys
+//
+//  Created by mmakankov on 04/06/14.
+//  Copyright (c) 2014 mmakankov. All rights reserved.
+//
+
+#import "MCProduct.h"
+
+@implementation MCProduct
+
+@end
